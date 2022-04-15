47f6bf2310ddefa61e93b163b53e18cc30a036d7
BobbyDev21 <bobby.dev2021@gmail.com>
2022-04-15 01:54:20 +0000

    Initial Upload

---
+++ havoc_gt5note10lte.mk
@@ -0,0 +1,17 @@
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/havoc.mk)
+
+$(call inherit-product, device/samsung/gt5note10lte/device.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 768
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gt5note10lte
+PRODUCT_NAME := havoc_gt5note10lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P555
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
