a75a34ceb89a83a82e27fe5079dcca158ae53eea
BobbyDev21 <bobby.dev2021@gmail.com>
2022-04-18 16:25:58 +0000

    Initial Upload

---
+++ system_prop.mk
@@ -0,0 +1,19 @@
+# Google Play Store
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=196608
+
+PRODUCT_PACKAGES += \
+	FlipFlap
+
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.cabl=0
+
+# RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=false
+
+# Screen
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
